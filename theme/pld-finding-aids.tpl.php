53f3cc296a459ce03d26abb49837acc0beda7938
Digital Content Coordinator <dcc@Digitals-iMac.local>
2018-12-12 09:46:02 -0500

    changed display of finding aids, added more data to variables array - add TN datastream - add link to collection - added description if available - changed css of template display

--- theme/pld-finding-aids.tpl.php
+++ theme/pld-finding-aids.tpl.php
@@ -4,17 +4,28 @@
 */
 
 ?>
-<h2 class="fa-page-title"><?php print t('Finding Aids'); ?></h2>
 <div class="pld-finding-aids__wrapper">
 	<?php foreach($items as $item) {
 		$label = $item['label'];
 		$pid = $item['pid'];
-		$fa = $item['finding_aid']; ?>
+		$fa = $item['finding_aid']; 
+		$description = $item['description']; 
+		$tn_image = $item['thumbnail_image'];
+		$url = $item['collection_url'];
+		?>
 	<div class="collection-finding-aid">
-		<a href="<?php print $fa; ?>"><i class="fas fa-file" aria-hidden="true"></i>
-		</a>
-		<a href="islandora/object/<?php print $pid?>" class="collection-title"><?php print $label; ?></a>
-		<a href="<?php print $fa; ?>" class="btn">View Finding Aid <i class="fas fa-angle-right" aria-hidden="true"></i></a>
+		<div class="collection_thumb">
+			<a href="<?php print $fa; ?>">
+				<img src="<?php print $tn_image; ?>" alt="<?php print $label; ?> thumbnail" />
+			</a>
+		</div>
+
+		<div class="collection_information">
+			<a href="islandora/object/<?php print $pid?>" class="collection-title"><?php print $label; ?></a>
+			<p class="collection_description"><?php print $description; ?></p>
+			<a href="<?php print $fa; ?>" class="btn">Download Finding Aid <i class="fas fa-file-download" aria-hidden="true"></i></a>
+			<a href="<?php print $url; ?>" class="btn"> View Collection <i class="fas fa-angle-right" aria-hidden="true"></i></a>
+		</div>
 	</div>
 	<?php
 		}
